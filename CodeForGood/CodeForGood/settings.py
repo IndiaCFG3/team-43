1fab85f6fe0a53648ea208c7049b3343309ca73f
Tanvi Aditya <tanviaditya1@gmail.com>
2020-08-16 21:21:19 +0530

    Update

--- CodeForGood/CodeForGood/settings.py
+++ CodeForGood/CodeForGood/settings.py
@@ -123,8 +123,7 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_URL='login'
 LOGIN_REDIRECT_URL='dashboard'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
