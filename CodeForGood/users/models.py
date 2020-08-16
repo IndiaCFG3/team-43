7d4a208f61b69af663fc892b52a4c5837a8addfc
Utkarsh Nigam <prateek.nigam.581@gmail.com>
2020-08-16 14:37:16 +0530

    Added the models

--- CodeForGood/users/models.py
+++ CodeForGood/users/models.py
@@ -7,3 +7,22 @@ class Profile(models.Model):
     lastname=models.CharField(max_length=10,default='Last name')
     usertype=models.CharField(max_length=10,default='admin')
     phone_number=models.CharField(max_length=10,null=False, blank=False, unique=True)
+
+class Student(models.Model):
+    student_id = models.CharField(max_length = 10)
+    student_name = models.TextField()
+    score = models.IntegerField()
+    rating = models.IntegerField()
+    performance = models.TextField()
+
+class BatchStudent(models.Model):
+    student_id = models.OneToOneField(Student)
+    batch_id models.CharField()
+
+class Employee(models.Model):
+    employee_id = models.CharField(max_length = 10)
+    employee_name = models.TextField()
+    manager_name = models.TextField()
+    leaves_taken = models.IntegerField()
+    rating = models.IntegerField()
+
