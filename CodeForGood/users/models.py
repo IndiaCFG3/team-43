eeaef46753cec1b0ba50fac9340228e75f075f96
Tanvi Aditya <tanviaditya1@gmail.com>
2020-08-16 13:00:22 +0530

    Profile model

---
+++ CodeForGood/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    firstname=models.CharField(max_length=10,default='First name')
+    lastname=models.CharField(max_length=10,default='Last name')
+    usertype=models.CharField(max_length=10,default='admin')
+    phone_number=models.CharField(max_length=10,null=False, blank=False, unique=True)
